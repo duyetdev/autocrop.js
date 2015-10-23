3e80647887c258889ba9a1b7cfcef46e24158f8b
Van-Duyet Le <me@duyetdev.com>
2015-10-23 18:29:28 +0700

    Update demo site

--- demo/assets/js/demo.js
+++ demo/assets/js/demo.js
@@ -1,15 +1,5 @@
-var img = $('img');
-var t = performance.now();
-AutoCrop.crop(img, {}, function(result) {
+var img = $('img')[0];
+
+AutoCrop.crop(img, {debug: true}, function(result) {
     console.log(img.src, result);
-    var crop = result.topCrop,
-        canvas = $('<canvas>')[0],
-        ctx = canvas.getContext('2d');
-    canvas.width = options.width;
-    canvas.height = options.height;
-    ctx.drawImage(img, crop.x, crop.y, crop.width, crop.height, 0, 0, canvas.width, canvas.height);
-    $(img)
-        .after(canvas)
-        .parent()
-        .append($('<p>').text(JSON.stringify(crop.score)));
 });
